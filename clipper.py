dbd82c80ca32e46a2f0a0e6b6a8d2272aa151d67
rohit12043 <rohitsinha12043@gmail.com>
2026-01-10 02:09:48 +0530

    feat(api): initialize search endpoints and enforce aac stereo audio for browser playback

--- clipper.py
+++ clipper.py
@@ -1,7 +1,7 @@
 import subprocess, os
 
 def create_clip(input_path: str, output_path: str, start_time, duration):
-    cmd = ["ffmpeg", "-ss", str(start_time), "-i", input_path,  "-t", str(duration), "-c", "copy", "-y", output_path]
+    cmd = ["ffmpeg", "-ss", str(start_time), "-i", input_path,  "-t", str(duration), "-c:v", "copy", "-c:a", "aac","-ac", "2", "-y", output_path]
     subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
     if os.path.exists(output_path):
---
+++ main.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+import sqlite3
+import clipper 
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware, allow_origins=['*'], allow_methods=['*'], allow_headers=['*'],
+)
+DB_NAME = "vid_database.db"
+
+def get_video_path(video_id):
+    with sqlite3.connect(DB_NAME) as db:
+        cursor = db.cursor()
+        cursor.execute("SELECT path FROM Videos where id = ?", (video_id,))
+        row = cursor.fetchone()
+        return row[0] if row else None
+        
+@app.get('/search')
+def get_video_info(q: str):
+    results = []
+    with sqlite3.connect(DB_NAME) as db:
+        db.row_factory = sqlite3.Row
+        cursor = db.cursor()
+        
+        sql = """
+        SELECT Videos.id as video_id, Videos.filename, Subtitles.start_time, Subtitles.text_content
+        From Subtitles
+        JOIN Videos on Subtitles.video_id = Videos.id
+        JOIN Subtitles_Idx on Subtitles.id = Subtitles_Idx.rowid
+        WHERE Subtitles_Idx MATCH ?
+        ORDER BY rank
+        LIMIT 50;
+        """
+        cursor.execute(sql, (q,))
+        rows = cursor.fetchall()
+        
+        for r in rows:
+            results.append({
+                "video_id": r["video_id"],
+                "filename": r["filename"],
+                "start": r["start_time"],
+                "text": r["text_content"]
+            })
+    return {"count": len(results), "results": results}
+
+@app.get("/watch/{video_id}")
+def watch_clip(video_id: int, start: float):
+    video_path = get_video_path(video_id)
+    if not video_path: raise HTTPException(status_code=404, detail="Video not found")
+    output_filename = f"clip_{video_id}_{start}.mp4"
+    
+    success = clipper.create_clip(video_path, output_filename, start, 10)
+    
+    if success:
+        return FileResponse(output_filename, media_type="video/mp4")
+    else:
+        raise HTTPException(status_code=500, detail="FFmpeg failed to create clip")
